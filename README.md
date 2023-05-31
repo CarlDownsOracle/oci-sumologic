c65813fa16a8949fa9ae6f8d3f1ab0a098f393c5
Carl Downs <carl.downs@oracle.com>
2023-05-31 17:37:51 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,111 @@
+# OCI -> Sumologic
+
+##  Overview
+
+This sample implements a simple architecture for exporting OCI Logs to Sumologic.
+
+---
+# OCI
+
+Here are the steps to set up OCI.
+
+## OCI Compartment
+
+_Name: ABC_
+
+Create a compartment to contain the following:
+
+
+- Virtual Cloud Network
+- Application + Function
+- Service Connector
+
+## OCI Group
+
+_Name: functions-developers_
+
+Create a user group where we can assign developer related policies.   
+
+
+## OCI Policies
+
+See [common policies](https://docs.oracle.com/en-us/iaas/Content/Identity/Concepts/commonpolicies.htm).
+
+### Developer Policies
+
+Developer can create, deploy and manage Functions and Applications
+
+See [reference](https://docs.oracle.com/en-us/iaas/Content/Identity/Concepts/commonpolicies.htm#)
+
+    Allow group functions-developers to manage repos in tenancy
+    Allow group functions-developers to manage serviceconnectors in tenancy
+    Allow group functions-developers to manage logging-family in tenancy
+    Allow group functions-developers to manage functions-family in tenancy
+    Allow group functions-developers to use cloud-shell in tenancy
+    Allow group functions-developers to use virtual-network-family in tenancy
+
+    Allow service faas to read repos in tenancy where request.operation='ListContainerImageSignatures'
+
+
+### Service Connector Policies
+
+Service Connector can access Application + Function, Audit Logs
+
+    Allow any-user to manage functions-family in compartment ABC where all {request.principal.type='serviceconnector'}
+    Allow any-user to manage logging-family in compartment ABC where all {request.principal.type='serviceconnector'}
+
+
+#  Function
+
+[Quick Start guide on OCI Functions](https://docs.oracle.com/en-us/iaas/Content/Functions/Tasks/functionsquickstartguidestop.htm) before proceeding.
+
+We will need to build and deploy a function.  The above guide takes you step by step.
+
+These are the variables we need to set up in the Function Application.  
+
+Here are the supported variables:
+
+| Environment Variable        |    Default     | Purpose                                                                            |
+|-----------------------------|:--------------:|:-----------------------------------------------------------------------------------|
+| SUMOLOGIC_ENDPOINT          | not-configured | Sumologic API endpoint                                                             |
+| SEND_TO_SUMOLOGIC          |      True      | Set to False if you need to debug things on OCI side without sending to Sumologic. |
+| LOGGING_LEVEL               |      INFO      | Controls function logging outputs.  Choices: INFO, WARN, CRITICAL, ERROR, DEBUG    |
+
+
+
+
+# Troubleshooting
+
+### FunctionTimeOut
+
+If the Function times out, please change the
+[function timeout setting](https://docs.oracle.com/en-us/iaas/Content/Functions/Tasks/functionscustomizing.htm).
+
+
+---
+
+## References
+
+Please see these references for more details.
+
+
+### OCI IaaS Data Sources
+
+- [OCI Logging Service](https://docs.oracle.com/en-us/iaas/Content/Logging/Concepts/loggingoverview.htm)
+- [OCI Audit Service](https://docs.oracle.com/en-us/iaas/Content/Audit/Concepts/auditoverview.htm)
+- [OCI Monitoring Service](https://docs.oracle.com/en-us/iaas/Content/Monitoring/Concepts/monitoringoverview.htm)
+- [OCI Events Service](https://docs.oracle.com/en-us/iaas/Content/Events/Concepts/eventsoverview.htm)
+- [OCI CloudGuard](https://docs.oracle.com/en-us/iaas/cloud-guard/using/index.htm)
+
+### OCI IaaS Enabling Technologies
+
+- [OCI Service Connector Hub](https://docs.oracle.com/en-us/iaas/Content/Functions/Concepts/functionsoverview.htm)
+- [OCI Functions Service](https://docs.oracle.com/en-us/iaas/Content/Functions/Concepts/functionsoverview.htm)
+- [OCI Streaming Service](https://docs.oracle.com/en-us/iaas/Content/Streaming/Concepts/streamingoverview.htm)
+- [OCI DevOps Pipelines](https://docs.oracle.com/en/solutions/build-cicd-pipelines-devops-function/index.html)
+
+
+---
+## License
+Copyright (c) 2014, 2023 Oracle and/or its affiliates
+The Universal Permissive License (UPL), Version 1.0
---
+++ func.py
@@ -0,0 +1,143 @@
+#
+# oci-sumologic 1.0
+#
+# Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+import io
+import json
+import logging
+import os
+import base64
+import requests
+from fdk import response
+
+
+# Function banner giving configuration, payload details
+banner = "oci function : {} / event payload bytes : {} / sending to sumologic: {} / logging level: {}"
+
+# Sumologic environment variables (set in OCI Application Configuration)
+sumologic_endpoint = os.getenv('SUMOLOGIC_ENDPOINT', 'not-configured')
+is_sending = eval(os.getenv('SEND_TO_SUMOLOGIC', "True"))
+
+# Enable if the function will be processing events or logs passing through OCI Streaming
+is_oci_streaming_conversion_enabled = eval(os.getenv('OCI_STREAMING_CONVERSION_ENABLED', "True"))
+
+# Set all registered loggers to the configured log_level
+logging_level = os.getenv('LOGGING_LEVEL', 'INFO')
+loggers = [logging.getLogger()] + [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+[logger.setLevel(logging.getLevelName(logging_level)) for logger in loggers]
+
+# --------------------------------------------
+# Functions
+# --------------------------------------------
+
+
+def handler(ctx, data: io.BytesIO = None):
+    """
+    OCI Function Entrypoint
+    :param ctx: OCI Function context
+    :param data: message payload bytes object
+    :return: None
+    """
+
+    try:
+
+        log_body = data.getvalue()
+        logging.info(banner.format(ctx.FnName(), len(log_body), is_sending, logging_level))
+
+        if is_oci_streaming_conversion_enabled:
+            log_body = convert_oci_streaming_format(log_body)
+
+        post_to_sumologic(log_body)
+
+        return response.Response(ctx, response_data=json.dumps({"status": "Success"}),
+                                 headers={"Content-Type": "application/json"})
+
+    except Exception as err:
+        logging.error("Error in handler: {}".format(str(err)))
+        raise err
+
+
+def post_to_sumologic(body_bytes: bytes):
+    """
+    Sends each event to Sumologic
+    """
+
+    session = requests.Session()
+
+    try:
+        adapter = requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=10)
+        session.mount('https://', adapter)
+
+        http_headers = {'Content-type': 'application/json'}
+
+        event_list = json.loads(body_bytes)
+        if not isinstance(event_list, list):
+            event_list = [event_list]
+
+        for event in event_list:
+            post_response = session.post(sumologic_endpoint, data=json.dumps(event), headers=http_headers)
+
+            if post_response.status_code != 200:
+                raise Exception('error posting to API endpoint', post_response.text, post_response.reason)
+
+    finally:
+        session.close()
+
+
+def convert_oci_streaming_format(body_bytes: bytes):
+    """
+    This function detects if the body is OCI Streaming format and converts it as needed to remove OCI
+    Streaming preamble / wrapper JSON if that is the case.  Otherwise, it returns the original argument value.
+
+    :param body_bytes: fn message body
+    :return: converted / original payload
+    """
+
+    converted = list()
+    event_list = json.loads(body_bytes)
+
+    # The presence of 'stream', 'partition' and 'value' attributes per message indicate
+    # that the list of events are in Streaming format.
+
+    for event in event_list:
+        stream = event.get('stream')
+        partition = event.get('partition')
+        value = event.get('value')
+
+        if stream and partition and value:
+            bytes_value = base64.b64decode(value)
+            utf8_value = bytes_value.decode('utf-8')
+            converted.append(json.loads(utf8_value))
+
+        else:
+            logging.info('OCI Streaming format not detected')
+            return body_bytes
+
+    converted_bytes = bytes(json.dumps(converted), 'ascii')
+    logging.info('OCI Streaming format detected, conversion complete')
+
+    return converted_bytes
+
+
+def local_test_mode(filename):
+    """
+    Test routine
+    """
+
+    logging.info("testing {}".format(filename))
+
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        converted_bytes = bytes(json.dumps(data), 'ascii')
+        post_to_sumologic(body_bytes=converted_bytes)
+
+
+"""
+Local Testing 
+"""
+
+if __name__ == "__main__":
+    local_test_mode('test_data/test.json')
+    # local_test_mode('test_data/test-list.json')
